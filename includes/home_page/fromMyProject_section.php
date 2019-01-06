6b85eb00fb35ff498d385077c50894a5b21abc83
emreozcan3320 <emreozcan3320@gmail.com>
2019-01-06 13:32:07 +0300

    responsive home-page done

--- includes/home_page/fromMyProject_section.php
+++ includes/home_page/fromMyProject_section.php
@@ -1,12 +1,13 @@
 <div class="container fromMyProject_section">
     <div class="row">
-        <div class="col-md-8 offset-2">  
+        <div class="col-12 mx-auto col-md-8 offset-2">  
             <h1 class="h2 text-center text-dark title">FROM MY PROJECT</h1>
             <hr>
         </div>  
     </div>
 
     <div class="row flip-over-row">
+
         <div class="col-md-4 flip-container" ontouchstart="this.classList.toggle('hover');">
             <div class="flipper">
                 <div class="front" style="background: url(http://placehold.it/500x500) 0 0 no-repeat;">
--- index.php
+++ index.php
@@ -2,14 +2,13 @@
 <?php include "includes/style.php"; ?>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </header>
-<?php include 'includes/menu.php'; ?>
-<?php include 'includes/home_section.php'; ?>
-<?php include 'includes/aboutme_section.php'; ?>
-<?php include 'includes/experience_section.php'; ?>
-<?php include 'includes/fromMyBlog_section.php'; ?>
-<?php include 'includes/fromMyProject_section.php'; ?>
-<?php include 'includes/contact_section.php'; ?>
-
-<?php include 'includes/footer_section.php'; ?>
+<?php include 'includes/home_page/menu.php'; ?>
+<?php include 'includes/home_page/home_section.php'; ?>
+<?php include 'includes/home_page/aboutme_section.php'; ?>
+<?php include 'includes/home_page/experience_section.php'; ?>
+<?php include 'includes/home_page/fromMyBlog_section.php'; ?>
+<?php include 'includes/home_page/fromMyProject_section.php'; ?>
+<?php include 'includes/home_page/contact_section.php'; ?>    
+<?php include 'includes/home_page/footer_section.php'; ?>
 <?php include "includes/script.php"; ?>
 
